eac97326feee4f7b27f8656d45cb0e1d4ec15bdb
Fabio <fabiobarrosfagundes@gmail.com>
2019-04-23 17:22:58 -0300

    integração ao Mongo, mLAb

--- dist/infra/db.js
+++ dist/infra/db.js
@@ -4,10 +4,11 @@ const mongoose = require("mongoose");
 class Database {
     constructor() {
         // private DB_URl  = 'mongodb://localhost:17017/db_portal'
-        this.DB_URl = 'mongodb://link-db/db_portal';
+        //private DB_URl  = 'mongodb://link-db/db_portal'
+        this.DB_URl = 'mongodb://admin:ndqm08h1@ds145916.mlab.com:45916/noticias-api';
     }
     createConnection() {
-        mongoose.connect(this.DB_URl);
+        mongoose.connect(this.DB_URl, { useNewUrlParser: true });
     }
 }
 exports.default = Database;
--- infra/db.ts
+++ infra/db.ts
@@ -2,10 +2,11 @@ import * as mongoose from 'mongoose';
 
 class Database{
     // private DB_URl  = 'mongodb://localhost:17017/db_portal'
-    private DB_URl  = 'mongodb://link-db/db_portal'
+    //private DB_URl  = 'mongodb://link-db/db_portal'
+    private DB_URl  = 'mongodb://admin:ndqm08h1@ds145916.mlab.com:45916/noticias-api'
 
     createConnection(){
-        mongoose.connect(this.DB_URl);
+        mongoose.connect(this.DB_URl, { useNewUrlParser: true });
     }
 }
 
