67b72b23edd311bd999da5daaa1c81ea7273db30
Fabio <fabiobarrosfagundes@gmail.com>
2019-05-02 18:44:43 -0300

    DIST

--- dist/repository/atletasRepository.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-const atletasSchema_1 = require("../models/atletasSchema");
-exports.default = mongoose.model("news", atletasSchema_1.default);
