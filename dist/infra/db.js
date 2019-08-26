d92f57152e652c46d9f9cf0fa78c69b689ef962f
Fabio <fabinho.bnho@gmail.com>
2019-08-25 21:43:09 -0300

    private DB_URl  = 'mongodb://admin:admin123@ds145916.mlab.com:45916/noticias-api'

--- dist/infra/db.js
+++ dist/infra/db.js
@@ -8,7 +8,8 @@ class Database {
     constructor() {
         // private DB_URl  = 'mongodb://localhost:17017/db_portal'
         //private DB_URl  = 'mongodb://link-db/db_portal'
-        this.DB_URl = 'mongodb://admin:ndqm08h1@ds145916.mlab.com:45916/noticias-api';
+        // private DB_URl  = 'mongodb://admin:ndqm08h1@ds145916.mlab.com:45916/noticias-api'
+        this.DB_URl = 'mongodb://admin:admin123@ds145916.mlab.com:45916/noticias-api';
     }
     createConnection() {
         mongoose_1.default.connect(this.DB_URl, { useNewUrlParser: true });
--- infra/db.ts
+++ infra/db.ts
@@ -3,7 +3,8 @@ import  mongoose from 'mongoose';
 class Database{
     // private DB_URl  = 'mongodb://localhost:17017/db_portal'
     //private DB_URl  = 'mongodb://link-db/db_portal'
-    private DB_URl  = 'mongodb://admin:ndqm08h1@ds145916.mlab.com:45916/noticias-api'
+    // private DB_URl  = 'mongodb://admin:ndqm08h1@ds145916.mlab.com:45916/noticias-api'
+    private DB_URl  = 'mongodb://admin:admin123@ds145916.mlab.com:45916/noticias-api'
 
     createConnection(){
         mongoose.connect(this.DB_URl, { useNewUrlParser: true });
